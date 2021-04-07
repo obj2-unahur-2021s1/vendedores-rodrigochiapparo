749f1e24fb97db6383094f1f8355c1437526ec44
rodrigo <rchiapparo@gmail.com>
2021-04-07 13:18:26 -0300

    Se completo la parte de vendedor,el test y  se creo centro de distribucion

---
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
@@ -0,0 +1,18 @@
+package ar.edu.unahur.obj2.vendedores
+
+class CentroDistribucion() {
+
+  val vendedores = mutableListOf<Vendedor>()
+
+  fun agregarVendedor(vendedor:Vendedor){ vendedores.add(vendedor)}
+
+  fun esVendedorEstrella() = vendedores.maxBy{v -> v.puntajeCertificaciones()}
+
+  fun puedeCubrir(ciudad : Ciudad) = vendedores.any { it.puedeTrabajarEn(ciudad)}
+
+  fun esRobusto() = vendedoresFirmes().size > 3
+
+  fun vendedoresFirmes() = vendedores.filter { v -> v.esFirme() }
+
+  fun vendedoresGenericos() = vendedores.filter { v -> v.esGenerico()}
+}
--- src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
@@ -6,11 +6,16 @@ import io.kotest.matchers.booleans.shouldBeTrue
 
 class VendedorTest : DescribeSpec({
   val misiones = Provincia(1300000)
+  val buenosAires = Provincia(10000000)
   val sanIgnacio = Ciudad(misiones)
 
   describe("Vendedor fijo") {
     val obera = Ciudad(misiones)
     val vendedorFijo = VendedorFijo(obera)
+    val certificacion = Certificacion(esDeProducto = true , puntaje = 50)
+
+    vendedorFijo.agregarCertificacion(certificacion)
+
 
     describe("puedeTrabajarEn") {
       it("su ciudad de origen") {
@@ -20,12 +25,23 @@ class VendedorTest : DescribeSpec({
         vendedorFijo.puedeTrabajarEn(sanIgnacio).shouldBeFalse()
       }
     }
+
+    describe("esFirme"){
+      vendedorFijo.esFirme().shouldBeTrue()
+    }
+
+    describe("esInfluyente"){
+      vendedorFijo.esInfluyente().shouldBeFalse()
+    }
   }
 
   describe("Viajante") {
     val cordoba = Provincia(2000000)
     val villaDolores = Ciudad(cordoba)
     val viajante = Viajante(listOf(misiones))
+    val certificacion = Certificacion(esDeProducto = true , puntaje = 20)
+
+    viajante.agregarCertificacion(certificacion)
 
     describe("puedeTrabajarEn") {
       it("una ciudad que pertenece a una provincia habilitada") {
@@ -35,5 +51,66 @@ class VendedorTest : DescribeSpec({
         viajante.puedeTrabajarEn(villaDolores).shouldBeFalse()
       }
     }
+
+    describe("esFirme"){
+      viajante.esFirme().shouldBeFalse()
+    }
+
+    describe("esInfluyente"){
+      viajante.esInfluyente().shouldBeFalse()
+    }
   }
+
+  describe("comercioCorresponal") {
+    val hurlingham = Ciudad(buenosAires)
+    val avellaneda = Ciudad(buenosAires)
+    val caballito = Ciudad(buenosAires)
+    val palomar = Ciudad(buenosAires)
+    val haedo = Ciudad(buenosAires)
+    val comercio = ComercioCorresponsal(listOf(hurlingham, avellaneda, caballito, palomar, haedo))
+    val certificacion = Certificacion(esDeProducto = true, puntaje = 100)
+
+    comercio.agregarCertificacion(certificacion)
+
+    describe("puedeTrabajarEnCiudad"){
+      comercio.puedeTrabajarEn(hurlingham).shouldBeTrue()
+    }
+
+    describe("esFirme") {
+      comercio.esFirme().shouldBeTrue()
+    }
+
+    describe("esInfluyente") {
+      comercio.esInfluyente().shouldBeTrue()
+    }
+
+  }
+
+    describe("centroDistribucion") {
+      val centro = CentroDistribucion()
+      val hurlingham = Ciudad(buenosAires)
+      val viajante = Viajante(listOf(misiones))
+      val vendedorFijo = VendedorFijo(hurlingham)
+
+
+
+
+      centro.agregarVendedor(viajante)
+      centro.agregarVendedor(vendedorFijo)
+
+      describe("esVendedorEstrella"){
+        centro.esVendedorEstrella() == viajante
+      }
+
+      describe("puedeCubrirCiudad"){
+        centro.puedeCubrir(sanIgnacio).shouldBeTrue()
+      }
+
+      describe("esRobusto"){
+        centro.esRobusto().shouldBeFalse()
+      }
+
+
+    }
+
 })
